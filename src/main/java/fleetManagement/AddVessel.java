46ed309434a222074df4315ecef80c1553285ee5
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2025-10-12 22:19:39 +0600

    separate locators

--- src/main/java/fleetManagement/AddVessel.java
+++ src/main/java/fleetManagement/AddVessel.java
@@ -1,8 +1,12 @@
+/*
+1. Explicit wait
+2. Separate Locators
+ */
+
 package fleetManagement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,6 +16,15 @@ import java.time.Duration;
 
 
 public class AddVessel {
+
+    // Locators
+    private static final By Fleet_Management_Menu = By.cssSelector("div[data-section='fleet'] span");
+    private static final By Add_Vessel_Button = By.cssSelector("#add-vessel-btn");
+    private static final By Vessel_Name = By.cssSelector("#vessel-name");
+    private static final By Vessel_Type = By.cssSelector("#vessel-type");
+    private static final By Year_Build = By.cssSelector("#year-built");
+    private static final By Vessel_Capacity = By.cssSelector("#capacity");
+
     public static void main(String[] args) {
         WebDriver driver = new FirefoxDriver();
         driver.get("https://muntasir101.github.io/marineXpress/");
@@ -19,24 +32,26 @@ public class AddVessel {
         // Setup Explicit wait
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        WebElement FleetManagementMenu = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#add-vessel-btn")));
+        WebElement FleetManagementMenu = wait.until(ExpectedConditions.elementToBeClickable(Fleet_Management_Menu));
         FleetManagementMenu.click();
 
-        WebElement AddVesselButton = driver.findElement(By.cssSelector("#add-vessel-btn"));
+        WebElement AddVesselButton = driver.findElement(Add_Vessel_Button);
         AddVesselButton.click();
 
-        WebElement VesselName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#vessel-name")));
+        WebElement VesselName = wait.until(ExpectedConditions.visibilityOfElementLocated(Vessel_Name));
         VesselName.sendKeys("Sea Super");
 
-        WebElement VesselType = driver.findElement(By.cssSelector("#vessel-type"));
+        WebElement VesselType = driver.findElement(Vessel_Type);
         Select select = new Select(VesselType);
         select.selectByValue("Tanker");
 
-        WebElement YearBuild = driver.findElement(By.cssSelector("#year-built"));
+        WebElement YearBuild = driver.findElement(Year_Build);
         YearBuild.sendKeys("2020");
 
-        WebElement Capacity = driver.findElement(By.cssSelector("#capacity"));
+        WebElement Capacity = driver.findElement(Vessel_Capacity);
         Capacity.sendKeys("20000");
+
+        driver.quit();
 
     }
 }
